a78cf1ea27b33584ee7f407c6a31c2cc3b94438e
crypt0 <acvjetan@gmail.com>
2021-05-11 10:44:24 +0200

    Add custom Typography text.

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -4,8 +4,8 @@ import Typography from "@material-ui/core/Typography";
 export default function Notes() {
   return (
     <div>
-      <Typography color="secondary" noWrap>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, provident. Quam quae, soluta beatae nulla aliquam totam. Nobis rerum sapiente corporis impedit fuga suscipit ut. Autem laborum dolor reprehenderit cupiditate repellendus sunt vel aliquam voluptates quos rem magnam cum voluptate, soluta distinctio nesciunt non temporibus praesentium vero, quam eos similique.
+      <Typography variant="h6" component="h2" color="textSecondary" gutterBottom>
+        Create a NewNote
       </Typography>
     </div>
   );
