b15d46554812b191bc1b302ac407d617d0ca4dd4
crypt0 <acvjetan@gmail.com>
2021-05-11 11:05:41 +0200

    Add Buttons and ButtonGroups.

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from '@material-ui/core/ButtonGroup'
 
 export default function Notes() {
   return (
@@ -7,6 +9,14 @@ export default function Notes() {
       <Typography variant="h6" component="h2" color="textSecondary" gutterBottom>
         Create a NewNote
       </Typography>
+      <Button type="submit">Submit</Button>
+      <Button type="submit" color="secondary" variant="outlined">Submit</Button>
+
+      <ButtonGroup color="secondary" variant="contained">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup>
     </div>
   );
 }
