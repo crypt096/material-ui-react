8a6223a05dc10fd7f3ac41f36457cab16ecbd42b
crypt0 <acvjetan@gmail.com>
2021-05-11 11:08:34 +0200

    Add Container and onClick event on button.

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import ButtonGroup from '@material-ui/core/ButtonGroup'
+import Container from '@material-ui/core/Container'
 
 export default function Notes() {
   return (
@@ -9,14 +9,10 @@ export default function Notes() {
       <Typography variant="h6" component="h2" color="textSecondary" gutterBottom>
         Create a NewNote
       </Typography>
-      <Button type="submit">Submit</Button>
-      <Button type="submit" color="secondary" variant="outlined">Submit</Button>
 
-      <ButtonGroup color="secondary" variant="contained">
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
-      </ButtonGroup>
+      <Button onClick={() => {
+        console.log('You clicked me')
+      }} type="submit" color="secondary" variant="contained">Submit</Button>
     </div>
   );
 }
