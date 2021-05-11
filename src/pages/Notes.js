d881f1e33238389467e6b99dc7f85bc01b953697
crypt0 <acvjetan@gmail.com>
2021-05-11 10:29:38 +0200

    Test Typography component.

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Typography from "@material-ui/core/Typography";
 
 export default function Notes() {
   return (
     <div>
-      Notes page
+      <Typography color="secondary" noWrap>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, provident. Quam quae, soluta beatae nulla aliquam totam. Nobis rerum sapiente corporis impedit fuga suscipit ut. Autem laborum dolor reprehenderit cupiditate repellendus sunt vel aliquam voluptates quos rem magnam cum voluptate, soluta distinctio nesciunt non temporibus praesentium vero, quam eos similique.
+      </Typography>
     </div>
-  )
+  );
 }
