18c770d8a6cfb01354ac65cfbdbc0e67c1b795c1
Aleksandar Cvjetan <31933549+crypt096@users.noreply.github.com>
2021-05-12 16:34:07 +0200

    Use makeStyles.

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -2,11 +2,19 @@ import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Container from '@material-ui/core/Container';
+import { makeStyles } from "@material-ui/core";
 
 // Icons
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 
 export default function Notes() {
+  const useStyles = makeStyles({
+    btn: {
+      fontSize: 60,
+      backgroundColor: 'violet',
+    }
+  })
+  
   return (
     <Container>
       <Typography variant="h6" component="h2" color="textSecondary" gutterBottom>
