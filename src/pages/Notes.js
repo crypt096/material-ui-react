7c39048f2710d608ee222cf99cd3ff4218cd1f57
Aleksandar Cvjetan <31933549+crypt096@users.noreply.github.com>
2021-05-12 19:21:47 +0200

    Add form.

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -3,14 +3,32 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Container from '@material-ui/core/Container';
 import { makeStyles } from "@material-ui/core";
+import TextField from '@material-ui/core/TextField';
 
 // Icons
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import { useState } from "react";
 
-const useStyles = makeStyles({})
+const useStyles = makeStyles({
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: 'block'
+  }
+})
 
 export default function Notes() {
   const classes = useStyles();
+  const [title, setTitle] = useState('')
+  const [details, setDetails] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(title && details) {
+      console.log(title, details)
+    }
+  }
 
   return (
     <Container>
@@ -18,7 +36,29 @@ export default function Notes() {
         Create a NewNote
       </Typography>
 
-      <Button type="submit" color="secondary" variant="contained" endIcon={<KeyboardArrowRightIcon/>}>Submit</Button>
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          onChange={(e) => setTitle(e.target.value)}
+          className={classes.field}
+          label="Note title"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+        />
+        <TextField
+          onChange={e => setDetails(e.target.value)}
+          className={classes.field}
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          multiline
+          rows={4}
+          fullWidth
+          required
+        />
+         <Button type="submit" color="secondary" variant="contained" endIcon={<KeyboardArrowRightIcon/>}>Submit</Button>
+      </form>
     </Container>
   );
 }
