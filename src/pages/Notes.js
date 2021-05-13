2002206a955d1f24cfcd47f0d41a993bb52390ec
Aleksandar Cvjetan <31933549+crypt096@users.noreply.github.com>
2021-05-13 11:26:10 +0200

    Use RadioButtons, RadioGroup and Form Control.

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -5,6 +5,13 @@ import Container from '@material-ui/core/Container';
 import { makeStyles } from "@material-ui/core";
 import TextField from '@material-ui/core/TextField';
 
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+
 // Icons
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 import { useState } from "react";
@@ -19,11 +26,12 @@ const useStyles = makeStyles({
 
 export default function Notes() {
   const classes = useStyles();
+  
   const [title, setTitle] = useState('')
   const [details, setDetails] = useState('')
-
   const [titleError, setTitleError] = useState(false)
   const [detailsError, setDetailsError] = useState(false)
+  const [category, setCategory] = useState("")
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -72,6 +80,18 @@ export default function Notes() {
           required
           error={detailsError}
         />
+
+        <FormControl className={classes.field}>
+          <FormLabel>Note Category</FormLabel>
+
+          <RadioGroup value={category} onChange={(e) => setCategory(e.target.value)}>
+            <FormControlLabel control={<Radio/>} label="Money" value="money" />
+            <FormControlLabel control={<Radio/>} label="Todos" value="todos" />
+            <FormControlLabel control={<Radio/>} label="Reminders" value="reminders" />
+            <FormControlLabel control={<Radio/>} label="Work" value="work" />
+          </RadioGroup>
+        </FormControl>
+
          <Button type="submit" color="secondary" variant="contained" endIcon={<KeyboardArrowRightIcon/>}>Submit</Button>
       </form>
     </Container>
