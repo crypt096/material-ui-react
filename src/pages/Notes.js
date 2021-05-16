104424daf3916ec0ab792414b6b6393887e22685
crypt0 <31933549+crypt096@users.noreply.github.com>
2021-05-16 18:40:50 +0200

    Add Notes.

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function Notes() {
+    useEffect(() => {
+        fetch('http://localhost:8000/notes')
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }, [])
+
     return (
         <div>
-            
+            Notes page
         </div>
     )
 }
