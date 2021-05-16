85aed068d05903dcc45e002f1691a034c7231dde
crypt0 <31933549+crypt096@users.noreply.github.com>
2021-05-16 18:46:29 +0200

    Fetch and display notes.

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,15 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Notes() {
+    const [notes, setNotes] = useState([])
+
     useEffect(() => {
         fetch('http://localhost:8000/notes')
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => setNotes(data))
     }, [])
 
     return (
         <div>
-            Notes page
+            {notes.map(note => (
+                <p key={note.id}>
+                    {note.title}
+                </p>
+            ))}
         </div>
     )
 }
